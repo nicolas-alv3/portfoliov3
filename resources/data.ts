6472c318913f81156b29102b91b28d34bcfc57f6
Nicolas Alvarez <nicolas.alv3@gmail.com>
2023-09-22 10:24:16 -0300

    add data to work experiences

--- resources/data.ts
+++ resources/data.ts
@@ -9,6 +9,7 @@ export enum Technologies {
     MONGO = "MongoDB",
     JS_REPORT = "JSreport",
     MYSQL = "Mysql",
+    SQLSERVER = "SQLServer",
     SPRINGBOOT = "Springboot",
     MATERIALUI = "MaterialUI",
     SEMANTICUI = "SemanticUI",
@@ -26,6 +27,7 @@ export const iconsMapping = {
     [Technologies.MONGO]: "",
     [Technologies.JS_REPORT]: "",
     [Technologies.MYSQL]: "",
+    [Technologies.SQLSERVER]: "",
     [Technologies.SPRINGBOOT]: "",
     [Technologies.MATERIALUI]: "",
     [Technologies.TDD]: "",
@@ -39,14 +41,43 @@ export interface Project {
     stack: Technologies[],
     link?: string,
     img: string,
+    testUrl?: string,
 }
 
 const redbee = {
     title: "Redbee",
-    shortDescription: "Short des",
-    description: "",
-    stack: [],
+    shortDescription: "Fullstack developer",
+    description: `
+    Led the transition from suboptimal practices to efficient development processes.\n
+    - Collaboratively developed fullstack solutions using Java and Angular.\n
+    - Fostered client engagement, ensuring the delivery of tailored solutions.\n
+    - Implemented industry best practices, enhancing code quality and project success`,
+    stack: [Technologies.JAVA, Technologies.ANGULAR, Technologies.SQLSERVER, Technologies.SPRINGBOOT],
+    img: "https://vfcomunicaciones.cl/wp-content/uploads/2019/04/redbee.jpg",
+    date: "Jun 22"
+}
+
+const factorIT = {
+    title: "FactorIT",
+    shortDescription: "Fullstack developer",
+    description: `
+    Developed REST web services in Java with Spring for Angular frontend.
+    - Worked on projects for BBVA Francés insurance.`,
+    stack: [Technologies.JAVA, Technologies.ANGULAR, Technologies.MYSQL, Technologies.SPRINGBOOT],
+    img: "https://ii.ct-stc.com/5/logos/empresas/2019/07/05/f977bb3068314eeea5bf170321314thumbnail.png",
     date: "Mar 21"
+}
+
+const ensolvers = {
+    title: "Ensolvers",
+    shortDescription: "Fullstack developer",
+    description: `
+    Migrated frontend from AngularJS to React per Figma UX-UI designs.
+    - Provided live support for bug resolution and customer inquiries.
+    - Contributed to backend development using Java and Spring Boot.`,
+    stack: [Technologies.JAVA, Technologies.REACT, Technologies.MYSQL, Technologies.SPRINGBOOT],
+    img: "https://uploads-ssl.webflow.com/646e6334b8ff4a650ca8ed68/647646c3d875dac6c8486195_Logo_Ensolvers_260px_footer_b_sin_frase_b.png",
+    date: "Jul 21"
 }
 
 const pomeloBebes: Project = {
@@ -56,7 +87,7 @@ const pomeloBebes: Project = {
     stack: [Technologies.NEXT_JS, Technologies.NODE, Technologies.NEST_JS, Technologies.MONGO],
     date: "Mar 21",
     img: "https://pomelobebes.vercel.app/_next/static/media/logo_pomelo_cuadrado.c8a2d125.png?imwidth=64",
-    link: "https://youtu.be/-ZIlGy2ew0g"
+    link: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6969355242471600128?compact=1"
 }
 
 const losHermanos: Project = {
@@ -65,8 +96,8 @@ const losHermanos: Project = {
     description: "Within the application, users can add, edit, and delete products. They can also adjust product stock from a dedicated tab and facilitate sales by identifying products through their codes and descriptions. Additionally, there is a product search feature that allows users to find items based on keyword similarity. In response to client requests, a section for price modification, both on a per-product and percentage basis, has been implemented. Currently, I am making minor enhancements to improve the user experience, as the program is used on a daily basis.",
     stack: [Technologies.KOTLIN, Technologies.REACT, Technologies.SPRINGBOOT, Technologies.MYSQL, Technologies.TDD],
     date: "2017",
-    img: "https://pomelobebes.vercel.app/_next/static/media/logo_pomelo_cuadrado.c8a2d125.png?imwidth=64",
-    link: "https://youtu.be/hij2fPb0EHY"
+    img: "https://raw.githubusercontent.com/nicolas-alv3/Corralon-y-ferreteria-los-dos-hermanos/master/frontend/src/icons/logo-horizontal.png",
+    link: "https://www.youtube.com/embed/-ZIlGy2ew0g?si=bDHBpNicPYAaQS6Y"
 }
 
 const distribuidoraJd: Project = {
@@ -76,17 +107,20 @@ const distribuidoraJd: Project = {
     stack: [Technologies.REACT, Technologies.JAVA, Technologies.JS_REPORT, Technologies.MYSQL, Technologies.MATERIALUI, Technologies.SPRINGBOOT, Technologies.TDD],
     date: "2016",
     img: "https://raw.githubusercontent.com/nicolas-alv3/DistribuidoraJDFront/master/src/icons/logo.png",
-    link: "https://youtu.be/-ZIlGy2ew0g"
+    link: "https://www.youtube.com/embed/-ZIlGy2ew0g?si=bDHBpNicPYAaQS6Y"
 }
 
 const glistore: Project = {
     title: "Glistore",
     shortDescription: "Build your own store",
-    description: "Within the application, users can upload, edit, and delete products, as well as adjust their stock from a dedicated tab. The application allows for sales, with products identified by code and description. Additionally, users can search for products and view sales separately. Upon completing a sale, users can generate a sales receip using a JSReport template. Currently, I am working on developing the Balance and Purchases modules, with the future goal of implementing automatic purchase lists and monthly financial statements.",
-    stack: [Technologies.NEXT_JS, Technologies.NEST_JS, Technologies.MONGO, Technologies.SEMANTICUI],
+    description: "Glistore is the evolution of Pomelo Bebes project, It is a platform where you can create your own store and customize it 100%. Glistore provides you a brand new store with logo, colors, products, images, categories, all for free!",
+    stack: [Technologies.NEXT_JS, Technologies.NODE, Technologies.NEST_JS, Technologies.MONGO, Technologies.SEMANTICUI],
     date: "2016",
-    img: "https://raw.githubusercontent.com/nicolas-alv3/DistribuidoraJDFront/master/src/icons/logo.png",
-    link: "https://youtu.be/-ZIlGy2ew0g"
+    img: "https://glidersoftware.web.app/static/media/gliderlogo.72278e90.png",
+    link: "https://www.youtube.com/embed/-ZIlGy2ew0g?si=bDHBpNicPYAaQS6Y",
+    testUrl: "https://glistore.vercel.app"
 }
 
-export const projects: Project[] = [pomeloBebes, distribuidoraJd, losHermanos];
+export const projects: Project[] = [glistore, pomeloBebes, losHermanos, distribuidoraJd];
+
+export const jobs: Project[] = [redbee, ensolvers, factorIT]
